4c0de9c910d2f8bf08823ec11a61f13e04e55cf3
Ernest Eferetin <ernesteferetin@yahoo.com>
2024-09-19 09:46:12 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ernest Eferetin on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
